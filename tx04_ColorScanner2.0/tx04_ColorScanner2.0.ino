756963f9b1a9f382d67ef62a905a91e5b5a78f7f
Wei-Lin Chang <55982289+weilinchang28@users.noreply.github.com>
2022-11-16 00:35:03 +0000

    1115
    Minor Update
    All four Instrument sends 0 when no color detect

--- tx04_ColorScanner2.0/tx04_ColorScanner2.0.ino
+++ tx04_ColorScanner2.0/tx04_ColorScanner2.0.ino
@@ -80,7 +80,17 @@ void loop()
 {
     if (!huskylens.request()) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
     else if(!huskylens.isLearned()) Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
-    else if(!huskylens.available()) Serial.println(F("Blank")); // WHITE Canvas
+    // else if(!huskylens.available()) Serial.println(F("Blank")); // WHITE Canvas
+    /////
+    else if (!huskylens.available())
+    {
+      Serial.println ("0");
+      int text = 0;
+      RF24NetworkHeader header(master00);   // (Address where the data is going)
+      bool ok = network.write(header, &text, sizeof(text)); // Send the data
+      
+    }
+    /////
     else
     {
         while (huskylens.available())
